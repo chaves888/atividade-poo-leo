8a46771ce0d7532bc78d10e700dc75e743d8878a
Leonardo Chaves Batista da Silva <127910384+chaves888@users.noreply.github.com>
2023-10-27 22:00:55 -0300

    Add files via upload

--- AtividadePoo-Heranca-leonardo/src/main/java/zoo/aves/Papagaio.java
+++ AtividadePoo-Heranca-leonardo/src/main/java/zoo/aves/Papagaio.java
@@ -5,7 +5,7 @@ import zoo.Ave;
 public class Papagaio extends Ave {
 
     public void repetirPalavras(String palavra){
-        System.out.println(getNome() + " " + palavra);
+        System.out.println(getNome() + " falou  " + palavra);
 
     };
 }
